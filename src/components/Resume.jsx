fa4b83524ed739747a8b3dab39935803cb879888
vijay03842 <vijaykumarkj546@gmail.com>
2025-06-01 19:43:54 +0530

    modify the files

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import resumeImg from '../assets/Resume.jpg'
+import React from 'react';
+import resumeImg from '../assets/Resume.jpg'; // Make sure this path is correct
 
 function Resume() {
   return (
-    
-   <section className="container-fluid resume-bg py-5 " id={'Resume'}>
+    <section className="container-fluid resume-bg py-5" id="Resume">
       <div className="row align-items-center">
-        {/* Image Section */}
-        <div className="col-12 col-md-6 mb-4 mb-md-0 text-center">
-          <img className="img-fluid" style={{height:'500px', width:'500px'}}src={resumeImg } alt="About" />
+        <div className="col-12 col-md-6 text-center mb-4 mb-md-0">
+          <img
+            className="img-fluid"
+            style={{ height: '500px', width: '500px' }}
+            src={resumeImg}
+            alt="Resume Preview"
+          />
         </div>
-    
-        {/* Text Section */}
-        <div className="col-12 col-md-6 text-white d-flex flex-column justify-content-center  px-5">
-            <h1 className="fs-1 mb-3">Resume</h1>
-            <p>
-              you can view my resume <a className='btn btn-primary ' href="https://github.com/vijay03842/my-resume"> Download</a>
-            </p>
+        <div className="col-12 col-md-6 text-white d-flex flex-column justify-content-center px-5">
+          <h1 className="fs-1 mb-3">Resume</h1>
+          <p>
+            You can view my resume
+            <a
+              href="/resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ms-2"
+            >
+              <button className="btn btn-primary">Download</button>
+            </a>
+          </p>
         </div>
       </div>
-  </section>
-    
-  )
+    </section>
+  );
 }
 
-export default Resume
+export default Resume;
